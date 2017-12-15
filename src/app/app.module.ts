98a81d13a7256304a430621a489265aed90f93a2
nizam <nizam9>
2017-12-15 16:47:57 +0530

    nizam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { ProductComponent } from "./product/product.component";
+import { ProductFilterPipe } from "./product/product-filter.pipe";
+import { StarComponent } from "./shared_folder/star/star.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductFilterPipe,
+    StarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
